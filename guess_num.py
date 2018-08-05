94c5d5c4838973ab7ab44a1ded742b97de7f0bcf
Daniel Huang <dnlhuang@gmail.com>
2018-08-05 11:06:35 +0800

    add range hint

--- guess_num.py
+++ guess_num.py
@@ -12,7 +12,11 @@ while True:
 	count += 1 # count = count + 1
 	num = input('請猜數字:')
 	num = int(num)
-	if num == r:
+	if num > end:
+		print('超出猜測範圍')
+	elif num < sta:
+		print('小於猜測範圍')
+	elif num == r:
 		print('猜對了！')
 		print('這是你猜的第', count, '次')
 		break
